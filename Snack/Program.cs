95cb0822eab82d9eea660c733eb30094f9ea5e1e
Anastasia <karpovanastiya@mail.ru>
2024-10-03 21:35:27 +0300

    Snake

---
+++ Snack/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Snack
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			char key;
+			do
+			{
+				key = Console.ReadKey(true).KeyChar;
+				Console.WriteLine($"{(int)key}\t{key}");
+			} while (true);
+		}
+	}
+}
